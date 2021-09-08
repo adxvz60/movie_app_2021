7cc9c78843431d792f0eaa4d4f6fa6bee99ba36e
adxvz60 <ujo881@naver.com>
2021-09-08 12:34:26 +0900

    Add Potato component
    Potato 컴포넌트 추가하고, index.js /App.js 수정

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Potato from './Potao'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello react!!!!!!!!</h1>
+      <Potato />
+    </div>
+  );
+}
+
+export default App
