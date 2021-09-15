bccf681ff5c82656976b3a20ea1ec23af5adf74b
adxvz60 <ujo881@naver.com>
2021-09-15 10:44:49 +0900

    Edit Food
    Food컴포넌트에 props를 출력

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ function App() {
   return (
     <div className="App">
       <h1>Hello react!!!!!!!!</h1>
-      <Food fav = "Kimchi" something={true} papapa={['hello', 1, 2, true]}/>
+      <Food fav = "Kimchi" something={true} />
       
     </div>
   );
 }
 
 function Food(props){
-  console.log(props);
+  //console.log(props);
   return(
-  <h3>I love potato</h3>
+  <h3>I love {props.fav}</h3>
   );
 }
 
