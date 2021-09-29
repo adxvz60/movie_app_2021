f23f1845f8bd66dcac0d3b329f53a197566d291a
adxvz60 <ujo881@naver.com>
2021-09-29 12:20:25 +0900

    add class App
    App클래스 추가

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class App extends Component{
+    constructor(props){
+        super(props)
+        console.log('constructor')
+    }s
+    state={
+        count: 0
+    }
+}
