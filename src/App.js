df212d4209a0e9444db7f77d016fb5747c720e11
adxvz60 <ujo881@naver.com>
2021-10-06 10:21:10 +0900

    Add class and render
    클래스 및 reaner추가

--- src/App.js
+++ src/App.js
@@ -1,46 +1,20 @@
-import React, {Component} from 'react'
+//import React, {Component} from 'react'
+import React from "react";
 
-class App extends Component{
-    constructor(props){
+class App extends React.Component{
+    constructor(props) {
         super(props)
-        console.log('constructor')
-    }
-    state={
-        count: 0
+        console.log('constructor');
     }
 
-add = () => {
-    this.setSatate(current => ({count: this.state.count +1}))
-}
-
-minus = () => {
-    this.setSatate(current => ({count: this.state.count -1}))
-}
-
-componentDidMount() {
-    console.log('componentDidMount');
-}
-
-componentDidUpdate() {
-    console.log('componentDidUpdate');
-}
-
-componentWillUnmount() {
-    console.log('componentWillUnmount');
-}
-
-
-
-render(){
-    console.log('render')
-    return(
-        <div>
-            <h1>The number is : {this.state.count}</h1>
-            <button onClick={this.add}>Add</button>
-            <button onClick={this.minus}>Minus</button>
-        </div>
+    render(){
+        console.log('render')
+        return(
+            <div>
+                <h1>Hello</h1>
+            </div>
         )
     }
 }
 
-export default App
+export default App;
