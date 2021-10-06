0076542b74de18cc7b17f0a59775ba6a64bd9526
adxvz60 <ujo881@naver.com>
2021-10-06 11:44:58 +0900

    Edit componentDidMount
    componentDidMount()Timeout 삭제 및 axios추가

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 //import React, {Component} from 'react'
 import React from "react";
+import axios from "axios";
 
 class App extends React.Component{
     state = {
-        isLodaing: true
+        isLodaing: true,
+        movies: []
     }
 
     componentDidMount() {
-        setTimeout(() => {
-            this.setState({isLoading: false})
-        }, 5000)
+        //영화 데이터 로딩
+        axios.get('https:yts-proxy.now.sh/list_movies.json')
     }
 
     render(){
         const{ isLoading } = this.state
         return(
             <div>
-                {isLoading ? 'Loading...' : 'We are ready'}
+                {isLoading ? 'Loading...' : '영화 데이터 출력'}
             </div>
         )
     }
