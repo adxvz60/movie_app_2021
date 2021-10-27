c9cc9c1fb01a3bbdd1541f082a447707b763e376
adxvz60 <ujo881@naver.com>
2021-10-27 12:33:41 +0900

    Navigation컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import { HashRouter,Route } from 'react-router-dom'
 
 import './App.css'
 import About from './routes/About'
+import Home from './routes/Home'
+import Navigation from './components/Navigation'
 
 function App(){
     return(
         <HashRouter>
-            <Route path="/home" />
+            <Navigation />
+            <Route path="/" exact={true} component={Home}/>
             <Route path="/about"  component={About} />
         </HashRouter>
     )
