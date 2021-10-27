f36290a6f48d3ceed692f6f4aedf82b846e2afbd
adxvz60 <ujo881@naver.com>
2021-10-27 12:21:27 +0900

    About.js에 css요소 추가

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,7 +1,11 @@
 import './About.css'
 
 function About(){
-    return <span><h1>About this page: I built it because I love movies.</h1></span>
+    return( 
+       <div className='about__container'>
+        <h1>About this page: I built it because I love movies.</h1>
+        </div>
+    )
 }
 
 export default About;
