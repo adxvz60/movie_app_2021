2aa9a7432a37ce30e15f68f0ef7ea9cbb36bbd6c
adxvz60 <ujo881@naver.com>
2021-11-05 18:46:50 +0900

    props추가

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,6 +1,7 @@
 import './About.css'
 
-function About(){
+function About(props){
+    console.log(props)
     return( 
        <div className='about__container'>
         <h1>About this page: I built it because I love movies.</h1>
