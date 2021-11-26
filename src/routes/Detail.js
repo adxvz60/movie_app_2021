8c62780e7b01bb90797044c61f8ef9d4f6bb99c9
adxvz60 <ujo881@naver.com>
2021-11-26 22:17:59 +0900

    오류 수정

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,6 +1,6 @@
-import react from "react";
+import React from 'react'
 
-class Detail extends React.Component {
+class Detail extends React.Component{
     componentDidMount(){
         const{ location, history} = this.props
         if(location.state === undefined){
